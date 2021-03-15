8ce68fd4697f236321b0edc914a1a1249ac08a05
George <gms1980@icloud.com>
2021-03-14 18:03:26 -0700

    added fetch

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+fetch (
+    'https://app.ticketmaster.com/discovery/v2/events.json?keyword=devjam&source=universe&countryCode=US&apikey=wH9MUA889uOpKEnatwDqKHSN2IHFzJhS'
+    )
+            .then(function(response) {
+                console.log(response)
+              return response.json();
+            })
+            .then(function(data) {
+              console.log(data);
+            });
