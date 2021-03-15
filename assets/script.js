8a5c065a119480a430e12fb12441a96dd622f9a4
George <gms1980@icloud.com>
2021-03-15 13:41:25 -0700

    added additional fetch api link

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,29 @@ const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", () => {
 console.log(submitButton);
 })
-Fetch
+//Fetch for TicketMaster API Key wH9MUA889uOpKEnatwDqKHSN2IHFzJhS
 fetch (
     'https://app.ticketmaster.com/discovery/v2/events.json?keyword=devjam&source=universe&countryCode=US&apikey=wH9MUA889uOpKEnatwDqKHSN2IHFzJhS'
     )
             .then(function(response) {
-                console.log(response)
+                console.log(response);
               return response.json();
             })
             .then(function(data) {
               console.log(data);
             });
 
+//Fetch for last FM
+ fetch(
+     `http://www.last.fm/api/auth/?api_key=79000ec6a486b0cc93684413435a84c6`
+    )
+            .then(function(response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            });
             
             //var favorite = localStorage.getItem(".favorite");   
             //localStorage.getItem(favorite);
