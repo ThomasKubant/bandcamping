710203fdd0e2c8e82ebb0e67983d8b58d103194d
reanna swick <thomaskubant@gmail.com>
2021-03-15 19:00:27 -0700

    search bar redirects to artist page

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var search = function() {
+    var userInput = document.getElementById('artistSearch').value;
+    localStorage.setItem("search", userInput);
+    window.location.href = "./artist.html"
+}
+document.getElementById("searchBtn").addEventListener('click', search);
